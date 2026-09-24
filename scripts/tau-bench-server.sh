fd90651c569afb3954bc219b551f676d8cac5757
Allan Mogley <allanmogley@gmail.com>
2026-09-24 14:12:29 +0200

    (komondor): lauch apps with script

---
+++ scripts/tau-bench-server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+sbatch /home/nr_szww/nr_szetdk/allan_wanjala/komondor-hpc/komondor-hpc/files_slurm/llama-cpp-cuda.slurm
+sbatch /home/nr_szww/nr_szetdk/allan_wanjala/komondor-hpc/komondor-hpc/files_slurm/tau-bench.slurm
+
+
